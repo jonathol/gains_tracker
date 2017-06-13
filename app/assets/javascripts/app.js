6a55a2429c3ff7fde9fa6471d262dc520b9869f8
Jonathon Lin <jonathonlin93@yahoo.com>
2017-06-13 16:00:19 -0700

    add angular-devise

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,4 +1,4 @@
-angular.module('gainsTracker', ['ui.router', 'templates'])
+angular.module('gainsTracker', ['ui.router', 'templates', 'Devise'])
 .config([
   '$stateProvider',
   '$urlRouterProvider',
