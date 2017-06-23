592c73a56ce953c5c6ce6ba69cde2ac4505bdfe3
Jonathon Lin <jonathonlin93@yahoo.com>
2017-06-23 16:51:34 -0700

    finish tdee calculator

--- app/assets/javascripts/calc/calcCtrl.js
+++ app/assets/javascripts/calc/calcCtrl.js
@@ -6,23 +6,20 @@ angular.module('gainsTracker')
     $scope.weight = 0;
     $scope.bfp = 0;
 
-    $scope.$watchGroup(['units', 'weight', 'bfp'], function(newVal){
-      console.log(newVal);
-      if (newVal[0] === 'imperial') {
-        $scope.lbm = ((100-newVal[2])/100) * (newVal[1]/2.20462);
+    $scope.answer = 0;
+
+    $scope.calculate = function(units,weight,bfp,level){
+
+      if (units === 'imperial') {
+        $scope.lbm = ((100-bfp)/100) * (weight/2.20462);
       }
       else {
-        $scope.lbm = ((100-newVal[2])/100) * newVal[1];
+        $scope.lbm = ((100-bfp)/100) * weight;
       }
-    });
-
-
-
-
-    $scope.bmr = 370 + (21.6 * $scope.lbm);
-    $scope.result = function() {
-
-      return $scope.lbm;
+      $scope.bmr = 370 + (21.6 * $scope.lbm);
+      $scope.answer = Math.round($scope.bmr*level);
     };
+
+
   }]
 );
