46d46e7181b38641b3399f7658413ce0bdd63e9f
Jonathon Lin <jonathonlin93@yahoo.com>
2017-06-22 17:20:19 -0700

    fix calculator nan bug

--- app/assets/javascripts/calc/calcCtrl.js
+++ app/assets/javascripts/calc/calcCtrl.js
@@ -2,11 +2,27 @@ angular.module('gainsTracker')
 .controller('CalcCtrl', [
   '$scope',
   function($scope) {
-    $scope.units = "imperial"
-    $scope.lbm = ((100 - $scope.bfp)/100) * $scope.weight;
+    $scope.units = 'imperial';
+    $scope.weight = 0;
+    $scope.bfp = 0;
+
+    $scope.$watchGroup(['units', 'weight', 'bfp'], function(newVal){
+      console.log(newVal);
+      if (newVal[0] === 'imperial') {
+        $scope.lbm = ((100-newVal[2])/100) * (newVal[1]/2.20462);
+      }
+      else {
+        $scope.lbm = ((100-newVal[2])/100) * newVal[1];
+      }
+    });
+
+
+
+
     $scope.bmr = 370 + (21.6 * $scope.lbm);
     $scope.result = function() {
-      return $scope.units;
+
+      return $scope.lbm;
     };
   }]
 );
