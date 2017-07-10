3e4c00fd4e7e807d3537977196bb1925d5ecf783
Jonathon Lin <jonathonlin93@yahoo.com>
2017-07-10 16:17:46 -0700

    add postgreSQL

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170710224720) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "exercises", force: :cascade do |t|
     t.string   "name"
     t.integer  "sets"
@@ -20,7 +23,7 @@ ActiveRecord::Schema.define(version: 20170710224720) do
     t.integer  "workout_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["workout_id"], name: "index_exercises_on_workout_id"
+    t.index ["workout_id"], name: "index_exercises_on_workout_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -37,9 +40,9 @@ ActiveRecord::Schema.define(version: 20170710224720) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "username"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["username"], name: "index_users_on_username", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+    t.index ["username"], name: "index_users_on_username", unique: true, using: :btree
   end
 
   create_table "workouts", force: :cascade do |t|
