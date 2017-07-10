80cd8f416667bf03bc1aeb7a0b0b9420914d3aa5
Jonathon Lin <jonathonlin93@yahoo.com>
2017-07-10 15:54:29 -0700

    add workout and exercsie model

---
+++ db/migrate/20170710224720_create_workouts.rb
@@ -0,0 +1,11 @@
+class CreateWorkouts < ActiveRecord::Migration[5.0]
+  def change
+    create_table :workouts do |t|
+      t.datetime :date
+      t.string :workout
+      t.string :notes
+      t.string :length
+      t.timestamps
+    end
+  end
+end
